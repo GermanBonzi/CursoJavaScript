c20bd919bc8cabb724160383c84420e49ac5a765
GermanBonzi <gbonzi716@gmail.com>
2020-07-28 15:29:36 -0300

    cursojavascriptmsj2

--- InsertarYborrarElementos.js
+++ InsertarYborrarElementos.js
@@ -3,7 +3,7 @@ const list = document.getElementById('list');
 const nuevoelemento = document.createElement('li');
 
 nuevoelemento.textContent='soy el elemento nuevo';
-
+nuevoelemento.textContent='soy el segundo elemento';
 
 //list.appendChild(nuevoelemento); // inserta el elemento en la ultima fila de la lista
 
